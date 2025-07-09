7b9d4b96db8501e221ddd8e0aa6492c2f856211d
lucas.ribeiro <lucas.riceirao11@gmail.com>
2025-07-08 21:21:00 -0300

    Adicionando tela de Game Over

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -22,14 +22,17 @@ function countDown() {
     state.view.timeLeft.textContent = state.values.currentTime;
 
     if(state.values.currentTime <= 0) {
-        clearInterval(state.actions.countDownTimerId);
-        clearInterval(state.actions.timerId);
-        alert("GAME OVER! O Seu resultado foi: " + state.values.result);
-    }
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+
+    document.getElementById("final-score").textContent = state.values.result;
+
+    document.getElementById("game-over").classList.remove("hidden");
+}
 }
 
-function playSound () {
-    let audio = new audio("../audios/hit.m4a");
+function playSound() {
+    const audio = new Audio("./assets/audios/hit.m4a");
     audio.volume = 0.2;
     audio.play();
 }
@@ -59,6 +62,20 @@ function addListenerHitBox() {
         });
     });
 } 
+function restartGame() {
+    // Oculta a tela de game over
+    document.getElementById("game-over").classList.add("hidden");
+
+    // Reinicia valores
+    state.values.result = 0;
+    state.view.score.textContent = 0;
+
+    state.values.currentTime = 60;
+    state.view.timeLeft.textContent = 60;
+
+    state.actions.timerId = setInterval(randomSquare, 1000);
+    state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
 
 
 function init() {
